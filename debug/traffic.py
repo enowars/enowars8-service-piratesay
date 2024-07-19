6235617dbcacfd98b695286784890d19345d8101
Christian Templen Grave <christgrave@gmail.com>
2024-07-19 09:39:00 +0200

    finish bambixploit_exploit2.py and add traffic.py to simulate traffic load

--- debug/traffic.py
+++ debug/traffic.py
@@ -1,10 +1,12 @@
 import socket
+import sys
 from concurrent.futures import ThreadPoolExecutor
 
-host = "localhost"
+host = sys.argv[1]
+connection_count = int(sys.argv[2])
 port = 4444
 
-matching_identities = [i for i in range(0, 100000)]
+matching_identities = [i for i in range(0, connection_count)]
 
 def connect(identity):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
